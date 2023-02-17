dfc20f5388209f4012ce19c8b4726409561ff7a1
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-18 01:43:21 +0600

    draggable div added

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 import { useDark, useToggle } from "@vueuse/core";
 const isDark = useDark();
 const toggleDark = useToggle(isDark)
+
+$(function () {
+  $("#draggable2").draggable({ axis: "x" });
+});
+
 </script>
 
 <template>
   <div class="bg-gray-300 dark:bg-slate-800 p-2 lg:p-5">
     <div class="flex relative">
-      <div class="w-24 h-80 lg:h-auto flex justify-center md:overflow-y-scroll lg:overflow-y-hidden sticky top-0">
+      <div class="w-16 md:w-20 lg:w-24 h-80 lg:h-auto flex justify-center md:overflow-y-scroll lg:overflow-y-hidden sticky top-0">
         <ul class="">
           <li
             class="my-1 cursor-pointer lg:my-7 text-xl text-slate-900 dark:text-slate-100 hover:bg-gray-400 dark:hover:text-slate-900 w-12 h-12 flex justify-center items-center rounded-full transition-all ease-in-out duration-500">
@@ -39,10 +44,10 @@ const toggleDark = useToggle(isDark)
           </li>
         </ul>
       </div>
-      <div class="px-5 w-full">
+      <div class="px-2 md:px-3 lg:px-5 w-full">
         <div class="md:flex justify-between items-center py-4">
           <div class="md:w-3/4">
-            <h1 class="text-4xl font-semibold text-slate-900 dark:text-slate-100">Jellyfish</h1>
+            <h1 class="text-2xl lg:text-4xl font-bold text-slate-900 dark:text-slate-100">Jellyfish</h1>
             <p class="text-xs pt-2 text-slate-900 dark:text-slate-100">16 Feb 10PM | 250MB | 00:15:30</p>
           </div>
           <div class="flex justify-between items-center mt-3 md:mt-0 w-full">
@@ -55,9 +60,12 @@ const toggleDark = useToggle(isDark)
               </div>
             </div>
             <div class="flex">
-              <div class="cursor-pointer mr-2 w-8 md:w-12 h-8 md:h-12 p-3 bg-gray-400 dark:bg-gray-700 rounded-full flex justify-center items-center">
-                <font-awesome-icon @click="toggleDark()" class="text-lg md:text-xl text-slate-900 dark:text-slate-100" icon="fa-regular fa-moon" />
-                <font-awesome-icon @click="toggleDark()" class="text-lg md:text-xl text-slate-900 dark:text-slate-100" icon="fa-regular fa-sun" />
+              <div
+                class="cursor-pointer mr-2 w-8 md:w-12 h-8 md:h-12 p-3 bg-gray-400 dark:bg-gray-700 rounded-full flex justify-center items-center">
+                <font-awesome-icon @click="toggleDark()" class="text-lg md:text-xl text-slate-900 dark:text-slate-100"
+                  icon="fa-regular fa-moon" />
+                <font-awesome-icon @click="toggleDark()" class="text-lg md:text-xl text-slate-900 dark:text-slate-100"
+                  icon="fa-regular fa-sun" />
               </div>
               <div
                 class="cursor-pointer mr-2 w-8 md:w-12 h-8 md:h-12 p-3 bg-gray-400 dark:bg-gray-700 rounded-full flex justify-center items-center">
@@ -155,14 +163,43 @@ const toggleDark = useToggle(isDark)
         </div>
       </div>
     </div>
-    <div>
-      <!-- <span class="flex h-3 w-3">
-        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-        <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-      </span> -->
+    <div class="h-24 relative">
+      <img class="shadow-lg rounded-2xl w-full h-full" src="../src/assets/jellyfish2.jpg" alt="">
+      <div id="draggable2" class="draggable ui-widget-content w-48 h-24 border border-slate-100 absolute mt-[-50px]"></div>
     </div>
   </div>
   <button @click="toggleDark()" class="">Click here</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.draggable {
+  /* width: 90px;
+  height: 90px; */
+  padding: 0.5em;
+  float: left;
+  margin: 0 10px 10px 0;
+}
+
+#draggable2 {
+  margin-bottom: 20px;
+}
+
+#draggable {
+  cursor: n-resize;
+}
+
+#draggable2 {
+  cursor: e-resize;
+}
+
+/* #containment-wrapper {
+  width: 95%;
+  height: 150px;
+  border: 2px solid #ccc;
+  padding: 10px;
+} */
+
+/* h3 {
+  clear: left;
+} */
+</style>
