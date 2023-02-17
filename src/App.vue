2b66e52024d556397935e49eece6df10b29ee683
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-17 17:50:06 +0600

    project setup done

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {useDark, useToggle} from "@vueuse/core";
+const isDark = useDark();
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <h1 class="text-[#000] dark:text-[#fff]">
+    Hello world!
+  </h1>
+  <button 
+  @click="toggleDark()"
+  class="">Click here</button>
+</template>
+
+<style scoped>
+
+</style>
