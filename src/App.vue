0c38242f681ffe5737965ff8aa3c4b6a6da6a456
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-17 22:13:38 +0600

    initial layout created

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,71 @@
 <script setup>
-import {useDark, useToggle} from "@vueuse/core";
+import { useDark, useToggle } from "@vueuse/core";
 const isDark = useDark();
 const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <h1 class="text-[#000] dark:text-[#fff]">
-    Hello world!
-  </h1>
-  <button 
-  @click="toggleDark()"
-  class="">Click here</button>
+  <div class="bg-gray-300 dark:bg-slate-800 p-5">
+    <div class="flex">
+      <div class="w-24 flex justify-center">
+        <ul class="">
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-qrcode" />
+          </li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-expand" />
+          </li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-regular fa-circle" />
+          </li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon
+              icon="fa-solid fa-arrow-trend-up" /></li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-text-width" />
+          </li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-maximize" />
+          </li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-circle-dot" />
+          </li>
+          <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"></li>
+        </ul>
+      </div>
+      <div class="px-5 w-full">
+        <div class=" flex justify-between py-4">
+          <div>
+            <h1 class="text-4xl font-semibold text-slate-900 dark:text-slate-100">Jellyfish</h1>
+            <p class="text-xs pt-2 text-slate-900 dark:text-slate-100">16 Feb 10PM | 250MB | 00:15:30</p>
+          </div>
+          <div class="text-6xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-circle-play" />
+          </div>
+          <div class="flex">
+            <div class="mr-2 w-16 h-16 p-3 bg-gray-400 dark:bg-gray-700 rounded-full flex justify-center items-center">
+              <font-awesome-icon class="text-4xl text-slate-900 dark:text-slate-100" icon="fa-solid fa-trash-can" />
+            </div>
+            <div class="w-16 h-16 p-3 bg-gray-400 dark:bg-gray-700 rounded-full flex justify-center items-center">
+              <font-awesome-icon class="text-4xl text-slate-900 dark:text-slate-100" icon="fa-solid fa-share-nodes" />
+            </div>
+          </div>
+        </div>
+        <div class="flex ">
+          <div class="w-9/12 mr-5"><img class="shadow-lg rounded-2xl" src="../src/assets/jellyfish.jpg" alt=""> </div>
+          <div
+            class="w-3/12 border border-slate-400  dark:border-slate-700 shadow-lg rounded-2xl p-5 bg-gray-400 dark:bg-gray-800">
+            <ul class="">
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-qrcode" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-expand" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-regular fa-circle" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-arrow-trend-up" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-text-width" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-maximize" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"><font-awesome-icon icon="fa-solid fa-circle-dot" /></li>
+              <li class="py-5 text-2xl text-slate-900 dark:text-slate-100"></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      hello
+    </div>
+  </div>
+  <button @click="toggleDark()" class="">Click here</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret, faCartShopping, faQrcode, faExpand, faArrowTrendUp, faTextWidth, faMaximize, faCircleDot, faCirclePlay, faTrashCan, faShareNodes} from '@fortawesome/free-solid-svg-icons'
+import { faCircle} from '@fortawesome/free-regular-svg-icons'
+// import { faGridHorizontal} from '@fortawesome/free-regular-svg-icons'
+
+
+/* add icons to the library */
+library.add(faUserSecret, faCartShopping, faQrcode, faExpand, faCircle, faArrowTrendUp, faTextWidth, faMaximize, faCircleDot, faCirclePlay, faTrashCan, faShareNodes)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
