6b4d3e6f8ecc81046ccb1fa487dc05ffede8ffde
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-17 23:32:30 +0600

    some more style added

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret, faCartShopping, faQrcode, faExpand, faArrowTrendUp, faTextWidth, faMaximize, faCircleDot, faCirclePlay, faTrashCan, faShareNodes} from '@fortawesome/free-solid-svg-icons'
-import { faCircle} from '@fortawesome/free-regular-svg-icons'
+import { faUserSecret, faCartShopping, faQrcode, faExpand, faArrowTrendUp, faTextWidth, faMaximize, faCircleDot, faCirclePlay, faTrashCan, faShareNodes, faCircleXmark} from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faSquare} from '@fortawesome/free-regular-svg-icons'
 // import { faGridHorizontal} from '@fortawesome/free-regular-svg-icons'
 
 
 /* add icons to the library */
-library.add(faUserSecret, faCartShopping, faQrcode, faExpand, faCircle, faArrowTrendUp, faTextWidth, faMaximize, faCircleDot, faCirclePlay, faTrashCan, faShareNodes)
+library.add(faUserSecret, faCartShopping, faQrcode, faExpand, faCircle, faArrowTrendUp, faTextWidth, faMaximize, faCircleDot, faCirclePlay, faTrashCan, faShareNodes, faSquare, faCircleXmark)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
